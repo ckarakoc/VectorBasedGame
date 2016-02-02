7b5bfbb6138253fd6a87ade06a3bf838ea23733d
Lars <lars.abbo@gmail.com>
2016-02-02 11:03:55 +0100

    Added basic structure

--- src/main/java/GameObject.java
+++ src/main/java/GameObject.java
@@ -3,4 +3,28 @@
  * Last update 2-2-2016
  */
 public class GameObject {
+
+    /**
+     * Creates a new GameObject.
+     */
+    public GameObject(){
+
+    }
+
+    /**
+     * Method to be overridden by developers to be able to add code to update(). afterUpdate() is called after update(). (Mind == blown)
+     */
+    public void afterUpdate(){
+
+    }
+
+    /**
+     * Updates the GameObject.
+     */
+    public final void update(){
+        //Update object
+
+        //Call afterUpdate()
+        afterUpdate();
+    }
 }
--- src/main/java/GameWorld.java
+++ src/main/java/GameWorld.java
@@ -1,6 +1,50 @@
+import java.util.ArrayList;
+
 /**
  * Created by Celal on 2-2-2016.
  * Last update 2-2-2016
  */
-public class GameWorld {
+public class GameWorld{
+
+    private ArrayList<GameObject> objects;
+
+    private int 
+
+    /**
+     * Creates a new GameWorld.
+     */
+    public GameWorld(){
+        this.objects = new ArrayList<GameObject>();
+    }
+
+    /**
+     * Adds the specified GameObject to this GameWorld.
+     * @param object The GameObject to be added.
+     */
+    public void add(GameObject object){
+        this.objects.add(object);
+    }
+
+    /**
+     * Removes the specified GameObject from this GameWorld.
+     * @param object The GameObject to be removed.
+     */
+    public void remove(GameObject object){
+        this.objects.remove(object);
+    }
+
+    /**
+     * Updates the GameWorld.
+     */
+    public final void update(){
+
+        afterUpdate();
+    }
+
+    /**
+     * Method to be overridden by developers to be able to add code to update(). afterUpdate() is called after update(). (Mind == blown)
+     */
+    public void afterUpdate(){
+
+    }
 }
