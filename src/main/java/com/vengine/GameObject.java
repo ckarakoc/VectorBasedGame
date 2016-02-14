f34a51e751d350599e19f8140966de8d6745de63
Lars <lars.abbo@gmail.com>
2016-02-14 16:39:40 +0100

    Added documentation

--- src/main/java/com/vengine/GameObject.java
+++ src/main/java/com/vengine/GameObject.java
@@ -93,60 +93,116 @@ public class GameObject {
         return vectors;
     }
 
+    /**
+     * Returns the X value of the coordinates that this GameObject is at.
+     * @return int
+     */
     public int getX(){
         return x;
     }
 
+    /**
+     * Sets the X value of the coordinates that this GameObject is at.
+     * @param x New value for x
+     */
     public void setX(int x){
         this.x = x;
     }
 
+    /**
+     * Returns the Y value of the coordinates that this GameObject is at.
+     * @return int
+     */
     public int getY(){
         return y;
     }
 
+    /**
+     * Sets the Y value of the coordinates that this GameObject is at.
+     * @param y New value for y
+     */
     public void setY(int y){
         this.y = y;
     }
 
+    /**
+     * The isTargetable property determines whether this GameObject can be hit by other GameObjects.
+     * @return boolean
+     */
     public boolean isTargetable() {
         return isTargetable;
     }
 
+    /**
+     * The isTargetable property determines whether this GameObject can be hit by other GameObjects.
+     * @param targetable New value for isTargetable
+     */
     public void setIsTargetable(boolean targetable) {
         isTargetable = targetable;
     }
 
+    /**
+     * The hasHealth property determines whether this GameObject has health and is therefore killable. Changing this property when isTargetable is true has no effect.
+     * @return boolean
+     */
     public boolean hasHealth() {
         return hasHealth;
     }
 
+    /**
+     * The hasHealth property determines whether this GameObject has health and is therefore killable. Changing this property when isTargetable is true has no effect.
+     * @param hasHealth New value for hasHealth
+     */
     public void setHasHealth(boolean hasHealth) {
         this.hasHealth = hasHealth;
     }
 
+    /**
+     * The doesCollideWithTerrain property determines whether this GameObject can move through terrain or not.
+     * @return boolean
+     */
     public boolean doesCollideWithTerrain() {
         return doesCollideWithTerrain;
     }
 
+    /**
+     * The doesCollideWithTerrain property determines whether this GameObject can move through terrain or not.
+     * @param doesCollideWithTerrain New value for doesCollideWithTerrain
+     */
     public void setDoesCollideWithTerrain(boolean doesCollideWithTerrain) {
         this.doesCollideWithTerrain = doesCollideWithTerrain;
     }
 
+    /**
+     * The doesCollideWithObjects property determines whether this GameObject can move through other GameObjects or not.
+     * @return boolean
+     */
     public boolean doesCollideWithObjects() {
         return doesCollideWithObjects;
     }
 
+    /**
+     * The doesCollideWithObjects property determines whether this GameObject can move through other GameObjects or not.
+     * @param doesCollideWithObjects New value for doesCollideWithObjects
+     */
     public void setDoesCollideWithObjects(boolean doesCollideWithObjects) {
         this.doesCollideWithObjects = doesCollideWithObjects;
     }
 
+    /**
+     * The isAffectedByGravity property determines whether this GameObject is affected by gravity.
+     * @return boolean
+     */
     public boolean isAffectedByGravity(){
         return this.isAffectedByGravity;
     }
 
-    public void setIsAffectedByGravity(boolean val){
-        this.isAffectedByGravity = val;
+    /**
+     * The isAffectedByGravity property determines whether this GameObject is affected by gravity.
+     * @param isAffectedByGravity New value for isAffectedByGravity
+     */
+    public void setIsAffectedByGravity(boolean isAffectedByGravity){
+        this.isAffectedByGravity = isAffectedByGravity;
     }
 
 
