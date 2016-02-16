78b40444bee66075ae840c5e0b2e45f247790a81
Lars <lars.abbo@gmail.com>
2016-02-16 17:09:47 +0100

    Added size methods

--- src/main/java/com/vengine/GameWorld.java
+++ src/main/java/com/vengine/GameWorld.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Created by Celal on 2-2-2016.
  * Last update 2-2-2016
  */
-public class GameWorld{
+public class GameWorld {
 
     //All GameObjects in this com.vengine.GameWorld
     private ArrayList<GameObject> objects;
@@ -20,37 +20,58 @@ public class GameWorld{
 
     //Some settings for a GameWorld
     private boolean hasGravity;
+    private ArrayList<GameObject> gameObjects;
 
     /**
-     * Creates a new com.vengine.GameWorld.
+     * Creates a new GameWorld.
+     * A GameWorld has these default properties:
+     * hasGravity: false
      */
-    public GameWorld(){
+    public GameWorld() {
         this.objects = new ArrayList<GameObject>();
+        this.hasGravity = false;
+    }
+
+    /**
+     * Creates a new GameWorld with specified size.
+     * A GameWorld has these default properties:
+     * hasGravity: true
+     *
+     * @param sizeX The size of the world in the x direction
+     * @param sizeY The size of the world in the y direction
+     */
+    public GameWorld(int sizeX, int sizeY) {
+        this.objects = new ArrayList<GameObject>();
+        this.hasGravity = false;
+        this.unitsX = sizeX;
+        this.unitsY = sizeY;
     }
 
     /**
      * Adds the specified com.vengine.GameObject to this com.vengine.GameWorld.
+     *
      * @param object The com.vengine.GameObject to be added.
      */
-    public void addGameObject(GameObject object){
+    public void addGameObject(GameObject object) {
         this.objects.add(object);
     }
 
     /**
      * Removes the specified com.vengine.GameObject from this com.vengine.GameWorld.
+     *
      * @param object The com.vengine.GameObject to be removed.
      */
-    public void removeGameObject(GameObject object){
+    public void removeGameObject(GameObject object) {
         this.objects.remove(object);
     }
 
     /**
      * Updates the com.vengine.GameWorld, also calls afterUpdate().
      */
-    public final void update(){
-        for(GameObject object : objects){
+    public final void update() {
+        for (GameObject object : objects) {
             //If gravity is enabled in this world, add the gravityVector to all GameObjects
-            if(hasGravity && object.isAffectedByGravity()){
+            if (hasGravity && object.isAffectedByGravity()) {
                 object.addVector(gravityVector);
             }
             object.update();
@@ -62,25 +83,30 @@ public class GameWorld{
 
     /**
      * Paints specified coordinates on specified Canvas.
-     * @param canvas
-     * @param x
-     * @param y
-     * @param width
-     * @param height
+     *
+     * @param canvas The canvas to be painted on
+     * @param x      The x coordinate of the top left corner of the frame in this GameWorld to be painted
+     * @param y      The y coordinate of the top left corner of the frame in this GameWorld to be painted
+     * @param width  The width of the frame to be painted
+     * @param height The height of the frame to be painted
      */
-    public void paint(Canvas canvas, int x, int y, int width, int height){
-        for(GameObject object : objects){
-            if(object.getX() >= x && object.getX() < x + width && object.getY() >= y && object.getY() < y + height){
+    public void paint(Canvas canvas, int x, int y, int width, int height) {
+        for (GameObject object : objects) {
+            if (object.getX() >= x && object.getX() < x + width && object.getY() >= y && object.getY() < y + height) {
                 //Object within boundaries
 
             }
         }
     }
 
+    public void paint(Object object, int x, int y, int width, int height) {
+
+    }
+
     /**
      * Method to be overridden by developers to be able to add code to update(). afterUpdate() is called after update(). (Mind == blown)
      */
-    public void afterUpdate(){
+    public void afterUpdate() {
 
     }
 
@@ -92,7 +118,7 @@ public class GameWorld{
         this.gravityVector = gravityVector;
     }
 
-    public boolean isHasGravity() {
+    public boolean hasGravity() {
         return hasGravity;
     }
 
@@ -100,7 +126,33 @@ public class GameWorld{
         this.hasGravity = hasGravity;
     }
 
-    public static GameWorld read(GameWorldFile file){
+    public static GameWorld read(GameWorldFile file) {
         return null;
     }
+
+    public ArrayList<GameObject> getGameObjects() {
+        return objects;
+    }
+
+    public void setSize(int x, int y) {
+        this.unitsX = x;
+        this.unitsY = y;
+    }
+
+    public void setSizeX(int x) {
+        this.setSize(x, this.unitsY);
+    }
+
+    public void setSizeY(int y) {
+        this.setSize(this.unitsX, y);
+    }
+
+    public int getSizeX() {
+        return this.unitsX;
+    }
+
+    public int getSizeY() {
+        return this.unitsY;
+    }
+
 }
